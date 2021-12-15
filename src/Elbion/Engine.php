24543d912d7f80f2ff11a55238cff7f8e33d7bb6
bariscodefx-php-god <85716242+bariscodefxy@users.noreply.github.com>
2021-12-15 23:58:07 +0300

    add simple engine code

--- src/Elbion/Engine.php
+++ src/Elbion/Engine.php
@@ -2,8 +2,43 @@
 
 namespace Elbion;
 
+/** The base engine */
 class Engine {
 
+	/** extension of template files (default .elb) */
+	protected $extension;
 
-	
+	/** view folder of template files */
+	protected $viewDir;
+
+	/** cache files of compiled files */
+	protected $cacheDir;
+
+	public function loadFile($filename)
+	{
+		ob_start();
+		require $this->viewDir . $filename . $extension;
+		$file = ob_get_clean();
+		echo($file);
+		return true;
+	}
+
+	public function setViewDir($viewFolder = "views/")
+	{
+		$this->viewDir = $viewFolder;
+		return true;
+	}
+
+	public function setCacheDir($cacheFolder = "cache/")
+	{
+		$this->cacheDir = $cacheFolder;
+		return true;
+	}
+
+	public function setExt($ext = ".elb")
+	{
+		$this->extension = $ext;
+		return true;
+	}
+
 }
